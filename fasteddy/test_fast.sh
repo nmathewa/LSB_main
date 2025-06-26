c7a7fa2bdbf9116527f43035602df65ca0f58c85
Nirmal <nalex2023@my.fit.edu>
2025-06-25 20:42:32 -0400

    added fasteddy pbs sh

--- fasteddy/test_fast.sh
+++ fasteddy/test_fast.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -A <ProjectAccount>
+#PBS -A UFIT0015
 #PBS -N FastEddy 
 #PBS -l select=1:ncpus=4:mpiprocs=4:ngpus=4:mem=100GB
 #PBS -l walltime=12:00:00
@@ -7,10 +7,10 @@
 #PBS -j oe
 #PBS -l job_priority=economy
 
-export BASEDIR=<path to code location>/FastEddy-model/
+export BASEDIR=/glade/u/home/nalex/fasteddy_test/FastEddy-model-3.0.0
 export SRCDIR=${BASEDIR}/SRC/FEMAIN
 export TUTORIALDIR=${BASEDIR}/tutorials/
-export EXAMPLE=<example .in filename>
+export EXAMPLE=Example04_BOMEX.in
 
 hostname
 module -t list
