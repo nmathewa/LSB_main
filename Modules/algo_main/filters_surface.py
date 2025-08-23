55d62af757eb5dafbd7a589bdc960bc291ea7b52
nmathewa <nirmalmathewmadathil@gmail.com>
2025-08-23 13:12:58 -0400

    LSB detector updates

---
+++ Modules/algo_main/filters_surface.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 11:13:56 2025
+
+@author: nalex2023
+"""
+
+
+class LSB_detector:
+    
+    def __init__(self,input_configs):
+        inconfig= input_configs
+        
+    
+    
+    def filter1_LSS_LSR(self,data1):
+        
